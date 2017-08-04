db557b21e442ef3bc77000fffd34d410a3fa9a06
Hazumon <Jackafella2@gmail.com>
2017-08-04 13:24:07 -0400

    updated controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,37 @@
 class EventsController < ApplicationController
 
   def index
+    @user = User.find(params[:id])
     @events = Event.all
   end
 
   def show
   end
+
+  def new
+    @user = User.find(params[:id])
+    @event = Event.new
+  end
+
+  def create
+    @user = User.find(params[:id])
+    @event = Event.new(review_params)
+
+    if @review.save
+      flash[:notice] = "Review added successfully"
+      # redirect_to restaurant_review_path(@review)
+       redirect_to restaurant_path(@user)
+    else
+      flash[:alert] = "Failed to save review."
+      render :new
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(
+    :name,
+    :location
+    )
+  end
+
 end

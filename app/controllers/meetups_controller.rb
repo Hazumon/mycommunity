e2132f3e50282cbd44b242836c55cc3b72f7a916
Hazumon <Jackafella2@gmail.com>
2018-03-21 18:33:25 -0500

    updated comments

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -2,9 +2,9 @@ class MeetupsController < ApplicationController
 
   def index
     # if using self with the methothod, you can access the Meetup class's method without using "new" ex: Meetup.meetups
-    Meetup.new.meetups
-    @meetups = Meetup.all
-    @meetups_list = @meetups.first(10)
+    Meetup.new.meetups # calls the method meetups. Class name Meetup followed by the method name: meetups
+    @meetups = Meetup.all # passes in all of the information from the method to the variable @meetups
+    @meetups_list = @meetups.first(11) # displays first 10 elements of of the information passed
   end
 
 end
